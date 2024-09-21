12544bdfe5ebb97a572ba0b2c163384861680a43
RafaelBorges22 <rafaelmascarenhasborges@gmail.com>
2024-09-21 12:45:45 -0300

    add controller

--- app/Controller/ProdutoController.php
+++ app/Controller/ProdutoController.php
@@ -1,3 +1,21 @@
 <?php
 
-namespace Src\Controller;
+namespace app\Controller;
+use app\Model\Produto;
+
+class ProdutoController{
+    private $produto;
+
+    public function _construct(Produto $model){
+        $this->produto = $model;
+    }
+public function create($data){
+    if (!isset($data->produtoId, $data->nome, $data->descricao)) {
+        # code...
+    }
+}
+
+}
+
+
+
